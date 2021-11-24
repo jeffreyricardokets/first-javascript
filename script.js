ad267528527faeeb057f62967212f017f4c48bd6
jeffreyricardokets <93093426+jeffreyricardokets@users.noreply.github.com>
2021-11-24 09:15:45 +0100

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy")
+let myName = "Jeffrey Kets";
+let age = 27;
+let ageString = "27";
+console.log(myName)
+console.log(4 + 4)
+console.log("4" + "4")
+console.log(4 + 4)
+console.log(4 - 4)
+console.log(4 * 4)
+console.log(4 / 4)
+console.log(4 % 4)
+console.log(typeof age)
+console.log(typeof ageString)
